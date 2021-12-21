edf7562feb6a2cef88ba85360b89d3380679f058
Daniel Lessa <daniellsf@id.uff.br>
2021-12-21 04:08:53 -0300

    Ajustando IPS na rede

--- client/main.py
+++ client/main.py
@@ -6,7 +6,7 @@ import cv2
 from interface import Interface
 import threading
 
-serverName = '192.168.56.1'
+serverName = '192.168.1.103'
 serverPort = 6000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 threads = list()
