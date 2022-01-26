3e5dea4c449b864d473f2aa7ceb973fae2809dbb
Juliana Moura <juliana_moura@id.uff.br>
2022-01-26 16:10:04 -0300

    Att fluxo de telas

--- client/interface.py
+++ client/interface.py
@@ -49,7 +49,7 @@ class Interface:
 
   def getVideos(self):
     general.formatSendTo(self.socket, general.CLIENT_COMMANDS[0], None, self.address)
-    
+
   def runVideo(self, video):
     general.formatSendTo(self.socket, general.CLIENT_COMMANDS[1], video, self.address)
 
@@ -60,7 +60,14 @@ class Interface:
   
   def showLogin(self):
     self.clearBody()
-    self.createBtn(self.body, "Login", self.showOptions)
+    self.createTitle(self.body, "LOGIN")
+    self.createTitle(self.body, "ID")
+    id = Entry(window).grid(row = 2,column = 0)
+    var1 = IntVar()
+    Checkbutton(master, text="Convidado", variable=var1).grid(row=2, sticky=W)
+    var2 = IntVar()
+    Checkbutton(master, text="Premium", variable=var2).grid(row=3, sticky=W)
+    self.createBtn(self.body, "Entrar", self.showOptions)
     self.createBtn(self.body, "Sair", self.stop)
   
   def showOptions(self):
@@ -86,15 +93,43 @@ class Interface:
   
   def showStatus(self):
     self.clearBody()
+    self.createTitle(self.body, "ID:")
+    self.createTitle(self.body, "CATEGORIA")
+    self.createTitle(self.body, "GRUPO")
     self.createBtn(self.body, "Voltar", self.showOptions)
 
   def showGroupOptions(self):
     self.clearBody()
-    self.createBtn(self.body, "Criar Grupo", self.showOptions)
-    self.createBtn(self.body, "Ver Grupo", self.showOptions)
-    self.createBtn(self.body, "Adicionar Usuário", self.showOptions)
-    self.createBtn(self.body, "Remover Usuário", self.showOptions)
+    self.createBtn(self.body, "Criar Grupo", self.showNewGroup)
+    self.createBtn(self.body, "Ver Grupo", self.showGroup)
+    self.createBtn(self.body, "Adicionar Usuário", self.showAddUser)
+    self.createBtn(self.body, "Remover Usuário", self.showRemoveUser)
     self.createBtn(self.body, "Voltar", self.showOptions)
+  
+  def showNewGroup(self):
+    self.clearBody()
+    self.createTitle(self.body, "NOVO GRUPO")
+    self.createTitle(self.body, "NOME DO GRUPO")
+    newGroup = Entry(window).grid(row = 2,column = 0)
+    self.createBtn(self.body, "Voltar", self.showGroupOptions)
+
+  def showGroup(self):
+    self.clearBody()
+    self.createTitle(self.body, "NOME DO GRUPO:")
+    self.createTitle(self.body, "MEMBROS DO GRUPO:")
+    self.createBtn(self.body, "Voltar", self.showGroupOptions)
+  
+  def showAddUser(self):
+    self.clearBody()
+    self.createTitle(self.body, "ID DO USUÁRIO PARA ADICIONAR:")
+    addUser = Entry(window).grid(row = 1,column = 0)
+    self.createBtn(self.body, "Voltar", self.showGroupOptions)
+
+  def showRemoveUser(self):
+    self.clearBody()
+    self.createTitle(self.body, "ID DO USUÁRIO PARA REMOVER:")
+    removeUser = Entry(window).grid(row = 1,column = 0)
+    self.createBtn(self.body, "Voltar", self.showGroupOptions)
 
   def run(self):
     self.root.mainloop()
